06fd1aeab87efb0bd7012b1f4c5d0d4350666c4c
Max Overbeek <s1129500@student.hsleiden.nl>
2024-10-09 14:39:39 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,27 @@
+schermfout = document.getElementById("fout");
+vraag1 = document.getElementById("vraag-1");
+vraag2 = document.getElementById("vraag-2");
+
+let index = 1; // Start met vraag 1
+let totaalVragen = 10; // Stel het aantal vragen in
+
+schermfout.style.display = "none";
+vraag2.style.display = "none";
+
+function antwoordFout() {
+    document.getElementById(`vraag-${index}`).style.display = "none";
+    schermfout.style.display = "block";
+}
+
+function volgendeVraag() {
+    // Verberg de huidige vraag
+    schermfout.style.display = "none";
+    index++;
+    
+    // Controleer of de volgende vraag bestaat
+    if (index <= totaalVragen) {
+        document.getElementById(`vraag-${index}`).style.display = "block";
+    } else {
+        console.log("Er zijn geen meer vragen");
+    }
+}
