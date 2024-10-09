5aea4c7378915edf18ea82f9d1cc6bc43a95bcab
Max Overbeek <s1129500@student.hsleiden.nl>
2024-10-09 14:54:41 +0200

    Heb functie wanner antwoord goed is toegevoegd

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 schermfout = document.getElementById("fout");
+schermGoed = document.getElementById("goed");
 vraag1 = document.getElementById("vraag-1");
 vraag2 = document.getElementById("vraag-2");
 
@@ -6,6 +7,7 @@ let index = 1; // Start met vraag 1
 let totaalVragen = 10; // Stel het aantal vragen in
 
 schermfout.style.display = "none";
+schermGoed.style.display = "none";
 vraag2.style.display = "none";
 
 function antwoordFout() {
@@ -13,8 +15,14 @@ function antwoordFout() {
     schermfout.style.display = "block";
 }
 
+function antwoordGoed() {
+    document.getElementById(`vraag-${index}`).style.display = "none";
+    schermGoed.style.display = "block";
+}
+
 function volgendeVraag() {
     // Verberg de huidige vraag
+    schermGoed.style.display = "none";
     schermfout.style.display = "none";
     index++;
     
